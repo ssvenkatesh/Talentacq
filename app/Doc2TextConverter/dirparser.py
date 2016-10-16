5c937f1633ac97fe795471935bc719baacb26bb7
S S Venkatesh <ssvenkatesh@gmail.com>
2016-10-16 14:27:02 -0400

    Added docx2Text parser

--- app/Doc2TextConverter/dirparser.py
+++ app/Doc2TextConverter/dirparser.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from docx import Document
 import os
+import zipfile
+from docxReader import get_docx_text 
+
 
 def readandprintfile(file):
-    print(file)
-    f = open(file,"r")
-    print(f.read())
-    f.close()
+    if file.endswith('.docx'):
+        print(file)
+        readDocx(file) 
+
+def readDocx(filepath):
+    pars=get_docx_text(filepath)
+    WriteToFile(pars)
+
+def WriteToFile(text):
+    file=open("output.txt","a")
+    file.write(text.encode('utf8'))
+    file.write('---------------')
+    file.close()
 
 def main():
     print("Reading file from dir")
